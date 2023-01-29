202f4565c4ec3482cefdee93d49564c4e4cd94f2
Anastasia Shcheblykina <anastasia23121993@gmail.com>
2023-01-29 17:05:27 +0300

    create repository

---
+++ src/main/java/Ticket.java
@@ -0,0 +1,36 @@
+public class Ticket {
+    private int id;
+    private int ticketPrice;
+    private String departureAirport;
+    private String arrivalAirport;
+    private int travelTime;
+
+    public Ticket(int id, int ticketPrice, String departureAirport, String arrivalAirport, int travelTime) {
+        this.id = id;
+        this.ticketPrice = ticketPrice;
+        this.departureAirport = departureAirport;
+        this.arrivalAirport = arrivalAirport;
+        this.ticketPrice = ticketPrice;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getTicketPrice() {
+        return ticketPrice;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public String getArrivalAirport() {
+        return arrivalAirport;
+    }
+
+    public int getTravelTime() {
+        return travelTime;
+    }
+}
---
+++ src/test/java/RepositoryTest.java
@@ -0,0 +1,85 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RepositoryTest {
+    Ticket ticket1 = new Ticket(1, 200, "GOJ", "LED", 180);
+    Ticket ticket2 = new Ticket(2, 400, "KUF", "LED", 160);
+    Ticket ticket3 = new Ticket(3, 600, "OGZ", "LED", 120);
+    Ticket ticket4 = new Ticket(4, 800, " FRU", "LED", 240);
+    Ticket ticket5 = new Ticket(5, 100, "MOW", "LED", 50);
+
+
+    @Test
+    public void addTicket() {
+        Repository repo = new Repository();
+
+        repo.add(ticket1);
+        repo.add(ticket2);
+        repo.add(ticket3);
+        repo.add(ticket4);
+        repo.add(ticket5);
+
+        Ticket[] expected = {ticket1, ticket2, ticket3, ticket4, ticket5};
+        Ticket[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void addOneTicket() {
+        Repository repo = new Repository();
+
+        repo.add(ticket1);
+
+        Ticket[] expected = {ticket1};
+        Ticket[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void addTwoTicket() {
+        Repository repo = new Repository();
+
+        repo.add(ticket4);
+        repo.add(ticket5);
+
+        Ticket[] expected = {ticket4, ticket5};
+        Ticket[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void deleteOneTicket() {
+        Repository repo = new Repository();
+
+        repo.add(ticket1);
+        repo.add(ticket2);
+        repo.add(ticket3);
+        repo.add(ticket4);
+        repo.add(ticket5);
+
+        Ticket[] expected = {ticket1, ticket2, ticket3, ticket4};
+        Ticket[] actual = repo.deletyById(5);
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void deleteOneToTicket() {
+        Repository repo = new Repository();
+
+        repo.add(ticket1);
+        repo.add(ticket2);
+        repo.add(ticket3);
+        repo.add(ticket4);
+        repo.add(ticket5);
+
+        Ticket[] expected = {ticket2, ticket3, ticket4, ticket5};
+        Ticket[] actual = repo.deletyById(1);
+    }
+
+}
+
